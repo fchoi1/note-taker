6fd846c495580fbd6c149f123ec2883bc1040132
fchoi1 <masterfabster2010@hotmail.com>
2022-01-01 20:24:36 -0500

    added some folder structure

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// The specificed folder to be readilty available (middleware)
+app.use(express.static('public'));
+
+// // Add the routes to app
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
